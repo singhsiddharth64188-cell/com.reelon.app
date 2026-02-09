48c1ccd6c4ed9dbb56361d47bc77fcd2b139a02d
singhsiddharth64188-cell <singhsiddharth64188@gmail.com>
2026-02-09 11:02:16 +0530

    Create reel_player.dart

---
+++ services/screens/screens/widgets/reel_player.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class ReelPlayer extends StatefulWidget {
+  final String videoUrl;
+  ReelPlayer({required this.videoUrl});
+
+  @override
+  _ReelPlayerState createState() => _ReelPlayerState();
+}
+
+class _ReelPlayerState extends State<ReelPlayer> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.videoUrl)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+        _controller.setLooping(true);
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width: _controller.value.size.width,
+                height: _controller.value.size.height,
+                child: VideoPlayer(_controller),
+              ),
+            ),
+          )
+        : Center(child: CircularProgressIndicator());
+  }
+}
