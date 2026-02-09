ce7e9e925d14f5a26431611661129f7c857ead4d
singhsiddharth64188-cell <singhsiddharth64188@gmail.com>
2026-02-09 10:59:31 +0530

    Create auth_service.dart

---
+++ services/auth_service.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signIn(String email, String password) async {
+    try {
+      var result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return result.user;
+    } catch (e) {
+      print("Login Error: $e");
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  bool isLoggedIn() {
+    return _auth.currentUser != null;
+  }
+}
